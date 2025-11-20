3d5aadcc63bafdfc7168c568893c3317afd9a18a
hnukui-lab <h.nukui@mac-shiobara.co.jp>
2025-11-20 14:16:56 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env
+    // Define env vars safely for client-side usage
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   }
 })
