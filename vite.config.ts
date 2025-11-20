d980ea0db9131d66603c7120aa625024756946ca
hnukui-lab <h.nukui@mac-shiobara.co.jp>
2025-11-20 14:43:23 +0900

    Add base path to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   define: {
     // Define env vars safely for client-side usage
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   }
 })
-
